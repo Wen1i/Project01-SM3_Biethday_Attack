f27516f92a08af3e348074c69ee7b1f2635b9b12
Wen1i <104118101+Wen1i@users.noreply.github.com>
2022-07-13 11:51:04 +0800

    Update sm3.py

--- sm3.py
+++ sm3.py
@@ -309,7 +309,7 @@ def Compress(w, IV):
 
 
 if __name__ == "__main__":
-    c = 'wwlwwlwwlabcdef0123456789876543210fedcba'  # 要加密的内容
+    c = 'abccba'  # 要加密的内容
     m = Fill(c)
     w = Expand(m)
     b = Iteration(m, w)
