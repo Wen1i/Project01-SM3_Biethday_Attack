e644148ce75ed807f586dc73005276618415d913
Wen1i <104118101+Wen1i@users.noreply.github.com>
2022-07-13 11:43:41 +0800

    Update birthday_attack.py

--- birthday_attack.py
+++ birthday_attack.py
@@ -6,8 +6,8 @@ for j in range(0,1000):
     for i in range(0,2**16):
         x = str(random.randint(0,2**256))
         y = str(random.randint(0,2**256))
-        x1 = mySM3.SM3(x,0)
-        y1 = mySM3.SM3(y,0)
+        x1 = main.SM3(x,0)
+        y1 = main.SM3(y,0)
         x2 = x1[0:8]
         y2 = y1[0:8]
         if(x2 == y2):
